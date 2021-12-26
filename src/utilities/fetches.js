53e524f4cc199773180daa92d401d1780723ecbb
marcosaccarola <marco.sacca.ads@gmail.com>
2021-12-26 01:48:35 +0100

    register fetch

--- src/utilities/fetches.js
+++ src/utilities/fetches.js
@@ -1,12 +1,32 @@
 const USER_URL='http://localhost:3000/user'
 
+//*______________________________________________ REGISTER
+export const register=async({email,pw,setUser})=>{
+    try {
+        const responseRegister=await fetch(USER_URL,
+            {
+                method:'POST',
+                body:JSON.stringify(email,pw),
+                headers:{'Content-Type':'application/json'}
+            })
+            if(responseRegister.ok){
+                let user=await responseRegister.json()
+                setUser(user)
+                console.log(user)
+            }else{
+                console.log('User not found.')
+            }
+    } catch (error) {
+        throw error
+    }
+}
 //*______________________________________________ LOGIN
 export const login=async({email,pw,setUser})=>{
     try {
         const responseUser=await fetch(USER_URL+'/login',
             {
                 method:'POST',
-                body:JSON.stringify({email,pw}),
+                body:JSON.stringify(email,pw),
                 headers:{'Content-Type':'application/json'}
             })
             if(responseUser.ok){
