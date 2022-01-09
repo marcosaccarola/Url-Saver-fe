efc12ef6980e9617d3d854d4e4f6fed4126456d9
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-09 19:31:37 +0100

    navbar

--- src/components/100-Bar.jsx
+++ src/components/100-Bar.jsx
@@ -1,20 +1,25 @@
 import { Col, Image, Row } from 'react-bootstrap'
 import './000.css'
 import logo from '../assets/logo.png'
+import { BsQuestionLg } from "react-icons/bs"
 
 const Bar=({user,setUser})=>{
 
     return(
         <>
             <Row className='bar bg-warning vw-100 align-items-center'>
-                <Col sm={2}>
-                    <img className='img-fluid bar bg-warning' src={logo} style={{}} />
+                <Col sm={3}>
+                    <img className='img-fluid bar bg-warning' src={logo}/>
+                    <span>Url Container</span>
                 </Col>
-                <Col sm={8}>
-                    home
+                <Col sm={6}>
+                    <BsQuestionLg />
                 </Col>
-                <Col sm={2}>
-                    avatar
+                <Col sm={3}>
+                    {
+                        user&&user.avatar&&
+                        <img src={user.avatar} />
+                    }
                 </Col>
             </Row>
         </>
