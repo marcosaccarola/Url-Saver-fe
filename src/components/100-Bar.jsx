b8333ce50fbd79d2e539643b220d96f9e6dc5cd9
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-10 23:20:25 +0100

    navbar react-bootstrap

--- src/components/100-Bar.jsx
+++ src/components/100-Bar.jsx
@@ -1,28 +1,46 @@
-import { Col, Container, Image, Navbar, Row } from 'react-bootstrap'
+import { Col, Container, Image, Nav, Navbar, Row } from 'react-bootstrap'
 import './000.css'
 import logo from '../assets/logo.png'
 import { BsQuestionLg } from "react-icons/bs"
+import { FiUserCheck } from "react-icons/fi"
+import {AiFillContainer} from 'react-icons/ai'
 
 const Bar=({user,setUser})=>{
 
     return(
         <>
-        <Navbar bg="warning" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img
-                    alt=""
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    />{' '}
-                Urls Containers
-                </Navbar.Brand>
-            </Container>
-        </Navbar>
 
-            {/* <Row className='bar bg-warning vw-100 align-items-center'>
+            {/* REACT-BOOTSTRAP */}
+            <Navbar bg="warning" variant="">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <AiFillContainer size={40} className='me-3' />
+                    Urls Containers
+                    </Navbar.Brand>    
+                    <Nav className="">
+                        {/* <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+                    </Nav>
+                    {
+                        user
+                        ?
+                            <Navbar.Brand href="#home">
+                                {user.email}
+                                <FiUserCheck size={40} className='ms-3' />
+                            </Navbar.Brand>
+                        :
+                            <Navbar.Brand href="#home" className='text-danger'>
+                                Login or Signin
+                                <FiUserCheck size={40} className='ms-3'/>
+                            </Navbar.Brand>
+                    }
+                </Container>
+            </Navbar>
+
+            
+            {/* ROWS & COLS
+            <Row className='bar bg-warning vw-100 align-items-center'>
                 <Col sm={3}>
                     <Container>
                         <img className='img-fluid bar bg-warning' src={logo}/>
@@ -38,7 +56,9 @@ const Bar=({user,setUser})=>{
                         <img className='img-fluid bar bg-warning' src={user.avatar} />
                     }
                 </Col>
-            </Row> */}
+            </Row>
+            */}
+
         </>
     )
 }
