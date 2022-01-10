422ab4b35deeace3946d9ceb587274a691442cf8
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-10 22:28:41 +0100

    navbar

--- src/components/100-Bar.jsx
+++ src/components/100-Bar.jsx
@@ -1,4 +1,4 @@
-import { Col, Image, Row } from 'react-bootstrap'
+import { Col, Container, Image, Navbar, Row } from 'react-bootstrap'
 import './000.css'
 import logo from '../assets/logo.png'
 import { BsQuestionLg } from "react-icons/bs"
@@ -7,10 +7,27 @@ const Bar=({user,setUser})=>{
 
     return(
         <>
-            <Row className='bar bg-warning vw-100 align-items-center'>
+        <Navbar bg="warning" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />{' '}
+                Urls Containers
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+
+            {/* <Row className='bar bg-warning vw-100 align-items-center'>
                 <Col sm={3}>
-                    <img className='img-fluid bar bg-warning' src={logo}/>
-                    <span>Urls Container</span>
+                    <Container>
+                        <img className='img-fluid bar bg-warning' src={logo}/>
+                        <span className=''>Urls Container</span>
+                    </Container>
                 </Col>
                 <Col sm={6}>
                     <BsQuestionLg />
@@ -21,7 +38,7 @@ const Bar=({user,setUser})=>{
                         <img className='img-fluid bar bg-warning' src={user.avatar} />
                     }
                 </Col>
-            </Row>
+            </Row> */}
         </>
     )
 }
