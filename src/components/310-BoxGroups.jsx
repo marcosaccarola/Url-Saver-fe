a293828e2f06f1c04d84e2ae59e9363443632091
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-11 01:04:52 +0100

    Update 310-BoxGroups.jsx

--- src/components/310-BoxGroups.jsx
+++ src/components/310-BoxGroups.jsx
@@ -3,18 +3,27 @@ import './000.css'
 
 const BoxGroups=({user,setSelectedGroup})=>{
 
+    const addContainer=()=>{
+        addGroup()
+    }
+
     return(
         <div className='mx-3 my-3 bg-dark text-light'>
             {user&&user.groups.map(g=>
                 <Row key={g._id}>
                     <Col sm={12} className='text-start'>
-                        <span className='container border border-light text-light' onClick={()=>setSelectedGroup(g)}>{g.name}</span>
+                        <span 
+                            className='container border border-light text-light' 
+                            onClick={()=>setSelectedGroup(g)}
+                            >{g.name}</span>
                     </Col>
                 </Row>
             )}
                 <Row>
                     <Col sm={12} className='text-start'>
-                        <span className='container  text-muted'>add group</span>
+                        <span 
+                            className='container text-muted'
+                            onClick={()=>addContainer()}>add container</span>
                     </Col>
                 </Row>
         </div>
