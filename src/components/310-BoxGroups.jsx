bc990df22a26b3af5291697e3aa3339554e5886e
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-11 11:02:17 +0100

    add group

--- src/components/310-BoxGroups.jsx
+++ src/components/310-BoxGroups.jsx
@@ -1,12 +1,21 @@
 import { Dropdown } from 'bootstrap'
 import {useState} from 'react'
-import { Button, ButtonGroup, Col, DropdownButton, Row } from 'react-bootstrap'
+import { Button, ButtonGroup, Col, DropdownButton, Form, Modal, Row } from 'react-bootstrap'
 import { postGroup } from '../utilities/fetches'
 import './000.css'
 
 const BoxGroups=({user,setSelectedGroup})=>{
-    const[newGroupName,setNewGroupName]=useState()
+    const[showGroupModal,setShowGroupModal]=useState(false)
+    const handleShowGroupModal=()=>{
+        console.log(showGroupModal)
+        showGroupModal==false?setShowGroupModal(true):setShowGroupModal(false)
+    }
 
+    const[newGroupName,setNewGroupName]=useState()
+    const handleGroupName=(e)=>{
+        setNewGroupName(e.target.value)
+    }
+    
     const addGroup=()=>{
         postGroup({"userId":user._id,"groupToAdd":{"name":newGroupName},setSelectedGroup})
     }
@@ -37,13 +46,25 @@ const BoxGroups=({user,setSelectedGroup})=>{
             {/* BUTTON GROUP */}
             <ButtonGroup vertical>
 
-                {user&&user.groups.map(g=>
-                    <Button key={g._id} onClick={()=>setSelectedGroup(g)}>{g.name}</Button>
-                )}
+            {user&&user.groups.map(g=>
+                <Button key={g._id} onClick={()=>setSelectedGroup(g)}>{g.name}</Button>
+            )}
 
-                <Button onClick={()=>addGroup()}>add Group</Button>
+            {showGroupModal==false
+            ?
+                <Button onClick={()=>handleShowGroupModal()}>add Group</Button>
+            :
+                <Form>
+                    <Form.Group as={Button} className="mb-3" controlId="formBasicEmail">
+                    <Form.Control type="text" placeholder="Group name" onChange={(e)=>handleGroupName(e)}/>
+                    </Form.Group>
+                    <Button onClick={()=>addGroup()}>save</Button>
+                    <Button onClick={()=>handleShowGroupModal()}>cancel</Button>
+                </Form>
+            }
 
             </ButtonGroup>
+            
 
         </>
         // </div>
