519fe4872106bd1bb5c93c43f6dedf8e5e8cbc90
marcosaccarola <marco.sacca.ads@gmail.com>
2022-01-16 01:45:38 +0100

    Navbar

--- src/components/100-Bar.jsx
+++ src/components/100-Bar.jsx
@@ -1,4 +1,4 @@
-import { Container, Navbar} from 'react-bootstrap'
+import { Container, Navbar, NavDropdown} from 'react-bootstrap'
 import './000.css'
 import { FiUserCheck } from "react-icons/fi"
 import {AiFillContainer} from 'react-icons/ai'
@@ -10,19 +10,28 @@ const Bar=({user,setUser})=>{
 
             <Navbar bg="light" variant="" >
                 <Container>
-                    <Navbar.Brand href="#">
+                    <Navbar.Brand className='text-primary'>
                         <AiFillContainer size={40} className='me-3' />
                         Urls Containers
                     </Navbar.Brand>
                 {
                 user
                 ?
-                    <Navbar.Brand href="#home">
+                <>
+                    <Navbar.Brand href="" className='d-flex align-items-center text-primary'>
                         {user.email}
-                        <FiUserCheck size={40} className='ms-3' />
+                        <NavDropdown title={user.email,<FiUserCheck size={40} className='text-primary' />} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                        {/* <FiUserCheck size={40} className='ms-3' /> */}
                     </Navbar.Brand>
+                </>
                 :
-                    <Navbar.Brand href="#home" className='text-danger'>
+                    <Navbar.Brand className='text-danger'>
                         Login or Signin
                         <FiUserCheck size={40} className='ms-3'/>
                     </Navbar.Brand>
